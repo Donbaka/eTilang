b282646cebb3244c39788fa3a6cdbf522226b779
Oka Suryanegara <okasurya@users.noreply.github.com>
2015-10-25 05:38:37 +0700

    voila

---
+++ TrafficHero/www/js/th.js
@@ -0,0 +1,10 @@
+$(function() {
+
+  Parse.$ = jQuery;
+
+  // Initialize Parse with your Parse application javascript keys
+  Parse.initialize("J5MRGJk7Z8xVwlZeoS2g3KSnfZZN7LWE61gjMSZA",
+                   "OhlmAugG00kdrM7Se5Qb0f0TFFQOq5uM0YKkuELP");
+
+  
+});
